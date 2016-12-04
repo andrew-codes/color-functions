7a4029e434d9b4c61861c6de0386745e564f48f7
Andrew Smith <andrew@andrew.codes>
2016-12-03 22:50:38 -0500

    Clamp luminance when lightening/darkening

--- src/functions/lighten.js
+++ src/functions/lighten.js
@@ -1,8 +1,10 @@
+import clamp from './../utilities/clamp';
 import toHsla from './../conversionFunctions/toHsla';
 import toRgba from './../conversionFunctions/toRgba';
 
 export default (color, amount) => {
     const hsla = toHsla(color);
     hsla.luminance += amount;
+    hsla.luminance = clamp(hsla.luminance, 0, 1);
     return toRgba(hsla);
 };
