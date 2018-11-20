7b527d623b31fd7e82f995ae09d618dc78e7ccc8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2018-11-20 18:56:34 -0500

    color inversion

---
+++ src/functions/invert.js
@@ -0,0 +1,16 @@
+
+import toHex from './../conversionFunctions/toHex';
+
+export default (hexString) => {
+    const hex = toHex(hexString).slice(1);
+    const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
+    const g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16);
+    const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
---
+++ src/functions/invert.spec.js
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+import invert from './invert';
+
+suite('invert', () => {
+    test('a color can be inverted producing the opposite color', () => {
+        const oppositeColor = invert('#fff');
+        expect(oppositeColor).to.equal('#000');
+    });
+});
