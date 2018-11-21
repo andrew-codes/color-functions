c2ef27360e3951f671d71f91e3115f579c35349a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2018-11-20 21:30:17 -0500

    lint rules

--- src/functions/invert.js
+++ src/functions/invert.js
@@ -1,6 +1,12 @@
 
 import toHex from './../conversionFunctions/toHex';
 
+function padZero(str, len) {
+    const length = len || 2;
+    const zeros = new Array(length).join('0');
+    return (zeros + str).slice(-length);
+}
+
 export default (hexString) => {
     const hex = toHex(hexString).slice(1);
     const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
@@ -8,9 +14,3 @@ export default (hexString) => {
     const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
     return `#${padZero(r)}${padZero(g)}${padZero(b)}`;
 };
-
-function padZero(str, len) {
-    const length = len || 2;
-    const zeros = new Array(length).join('0');
-    return (zeros + str).slice(-length);
-}
